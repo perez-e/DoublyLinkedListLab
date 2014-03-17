ffe1d3567f3456a4f521a3fb1e70cbf3b0e844d1
Eric Perez <ericricardoperez@gmail.com>
2014-03-17 12:16:06 -0700

    node specs green

--- node.rb
+++ node.rb
@@ -1,8 +1,10 @@
 # A simple Node class.  A LinkList will be composed of many
 # nodes.
 class Node
-  
+  attr_accessor :value, :next, :previous
   def initialize(value)
     @value = value
+    @next = nil
+    @previous = nil
   end
 end
